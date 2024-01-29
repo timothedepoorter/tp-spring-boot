00d95e3107793ce50124eb25e56d9f5fc5ef75d1
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-01-29 14:12:35 +0100

    Init project

---
+++ src/test/java/fr/timothe/tpspringboot/TpSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.timothe.tpspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
